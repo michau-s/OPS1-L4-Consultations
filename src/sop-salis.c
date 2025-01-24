f01c4f13543d67aaf50c6127788874d479bf541f
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2025-01-24 16:38:38 +0100

    Stage01

--- src/sop-salis.c
+++ src/sop-salis.c
@@ -1,5 +1,36 @@
 #include "common.h"
 
+typedef struct argsSignalHandler_t
+{
+    int* doWork;
+    pthread_mutex_t* mxDoWork;
+    sigset_t* mask;
+
+} argsSignalHandler_t;
+
+void* signal_handling(void* voidArgs)
+{
+    argsSignalHandler_t* args = voidArgs;
+    int signo;
+    for (;;)
+    {
+        if (sigwait(args->mask, &signo))
+            ERR("sigwait failed.");
+        switch (signo)
+        {
+            case SIGINT:
+                pthread_mutex_lock(args->mxDoWork);
+                *(args->doWork) = 0;
+                pthread_mutex_unlock(args->mxDoWork);
+                return NULL;
+            default:
+                printf("unexpected signal %d\n", signo);
+                exit(1);
+        }
+    }
+    return NULL;
+}
+
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
@@ -7,5 +38,44 @@ int main(int argc, char* argv[])
     int do_work = 1;
     pthread_mutex_t do_work_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-    usage(argc, argv);
+    if (argc != 3)
+        usage(argc, argv);
+
+    int N;
+    N = atoi(argv[1]);
+
+    if (N <= 0 || N > 20)
+        usage(argc, argv);
+
+    int Q;
+    Q = atoi(argv[2]);
+
+    if (Q <= 0 || Q > 10)
+        usage(argc, argv);
+
+    pthread_t sigID;
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);
+
+    argsSignalHandler_t sigArgs;
+    sigArgs.doWork = &do_work;
+    sigArgs.mask = &mask;
+    sigArgs.mxDoWork = &do_work_mutex;
+
+    pthread_create(&sigID, NULL, signal_handling, &sigArgs);
+
+    while (1)
+    {
+        pthread_mutex_lock(&do_work_mutex);
+        if (do_work == 0)
+        {
+            pthread_mutex_unlock(&do_work_mutex);
+            break;
+        }
+        pthread_mutex_unlock(&do_work_mutex);
+    }
+
+    pthread_join(sigID, NULL);
 }
